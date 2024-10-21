f0d5f967f4d41cb55bd62fd05973ceadfc4aaf9d
WaizWarden <ljurica506@gmail.com>
2024-10-21 21:31:34 +0200

    Created tileObject, that is stored in chunk array

--- MiniMX/WorldGeneration.cs
+++ MiniMX/WorldGeneration.cs
@@ -1,52 +1,153 @@
-using System;
-using Microsoft.Xna.Framework;
-using Microsoft.Xna.Framework.Graphics;
-using System.Collections.Generic;
-using Microsoft.Xna.Framework.Content;
+    using System;
+    using Microsoft.Xna.Framework;
+    using Microsoft.Xna.Framework.Graphics;
+    using System.Collections.Generic;
+    using Microsoft.Xna.Framework.Content;
 
-namespace MiniMX;
-public class WorldGeneration : Sprite
-{
-    private Texture2D tileSet;
-    private int tileWidth = 16;
-    private int tileHeight = 16;
-    private Dictionary<int, Texture2D> tilesDictionary = new Dictionary<int, Texture2D>();
-    
-    int[,] Tiles =
+    namespace MiniMX;
+
+    /*
+     * NAME CONVENTION
+     * tile - a single block - like one block in minecraft, every tile is an object (TileObject)
+     * Chunk - consits of tiles (chunkData) 32x32
+     * All chunks are stored in World 2d array and will load/unload when player is near
+     */
+
+    public class TileObject
     {
-        {0,1,1,1,1,1,1,1,1,1,1,1,2},
-        {3,4,4,4,4,4,4,4,4,4,4,4,5},
-    };
-    public void LoadTileSet(ContentManager Content, GraphicsDevice graphicsDevice, string fileName)
+        private bool collision; // if true player can walk throuch
+        private Texture2D texture;
+        private Vector2 position;
+        private int tileId;
+
+        public TileObject(bool Collision, Vector2 Position, Texture2D Texture, int TileId)
+        {
+            collision = Collision;
+            position = Position;
+            texture = Texture;
+            tileId = TileId;
+        }
+    }
+
+    public class WorldGeneration : Sprite
     {
-        tileSet = Content.Load<Texture2D>(fileName);
+        private Texture2D tileSet; // image of all tiles
+        private readonly int tileWidth = 16;
+        private readonly int tileHeight = 16;
+        private TileObject[,] chunk; // 2D array to store TileObjects for the current chunk
         
-        int tileIndex = 0;
-        for (int y = 0; y != tileSet.Height; y += tileHeight)
+        public WorldGeneration()
         {
-            for (int x = 0; x != tileSet.Width; x += tileWidth)
-            {
-                Texture2D tileTexture = new Texture2D(graphicsDevice, tileWidth, tileHeight);
-                Color[] tileData = new Color[tileWidth * tileHeight];
+            chunk = new TileObject[32, 32]; 
+        }
+        
+        private Dictionary<int, Texture2D>
+            tilesDictionary = new Dictionary<int, Texture2D>(); // stores all tiles and its ids
 
-                tileSet.GetData(0, new Rectangle(x, y, tileWidth, tileHeight), tileData, 0,
-                    tileData.Length);
-                tileTexture.SetData(tileData);
-                tilesDictionary.Add(tileIndex, tileTexture);
-                ++tileIndex;
-            }
-        }
-    }
-    public void Draw(SpriteBatch spriteBatch)
-    {
-        for (int row = 0; row < Tiles.GetLength(0); row++)
+
+        int[,] ChunkData =
         {
-            for (int col = 0; col < Tiles.GetLength(1); col++)
+            { 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 2 },
+            { 3, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 5 },
+            { 3, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 5 },
+            { 3, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 5 },
+            { 3, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 5 },
+            { 3, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 5 },
+            { 3, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 5 },
+            { 3, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 5 },
+            { 3, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 5 },
+            { 3, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 5 },
+            { 3, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 5 },
+            { 3, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 5 },
+            { 3, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 5 },
+            { 3, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 5 },
+            { 3, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 5 },
+            { 3, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 5 },
+            { 3, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 5 },
+            { 3, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 5 },
+            { 3, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 5 },
+            { 3, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 5 },
+            { 3, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 5 },
+            { 3, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 5 },
+            { 3, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 5 },
+            { 3, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 5 },
+            { 3, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 5 },
+            { 3, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 5 },
+            { 3, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 5 },
+            { 3, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 5 },
+            { 3, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 5 },
+            { 3, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 5 },
+            { 3, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 5 },
+            { 6, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 8 },
+        };
+
+
+        // This function create tilesDictionary of individual tiles (Texture2D tileSet) and gives them id,
+        public void LoadTileSet(ContentManager Content, GraphicsDevice graphicsDevice, string fileName)
+        {
+            tileSet = Content.Load<Texture2D>(fileName);
+
+            int tileIndex = 0;
+            for (int y = 0; y != tileSet.Height; y += tileHeight)
             {
-                int tileIndex = Tiles[row, col];
-                spriteBatch.Draw(tilesDictionary[tileIndex], new Vector2(col * 16, row * 16), Color.White);
+                for (int x = 0; x != tileSet.Width; x += tileWidth)
+                {
+                    Texture2D tileTexture = new Texture2D(graphicsDevice, tileWidth, tileHeight);
+                    Color[] tileData = new Color[tileWidth * tileHeight];
+
+                    tileSet.GetData(0, new Rectangle(x, y, tileWidth, tileHeight), tileData, 0,
+                        tileData.Length);
+                    tileTexture.SetData(tileData);
+                    tilesDictionary.Add(tileIndex, tileTexture);
+                    ++tileIndex;
+                }
+            }
+
+            DebugStoreLocalChunkDataToChunk(ChunkData);
+        }
+
+        /*
+         * FOR DEBUG ONLY
+         * Create a chunk from small world and stores it in World array
+         */
+        private void DebugStoreLocalChunkDataToChunk(int[,] ChunkData)
+        {
+            if (ChunkData.GetLength(0) != 32 || ChunkData.GetLength(1) != 32)
+            {
+                throw new ArgumentException("ChunkData must be a 32x32 array.");
+            }
+
+            for (int row = 0; row < ChunkData.GetLength(0); row++)
+            {
+                for (int col = 0; col < ChunkData.GetLength(1); col++)
+                {
+                    int tileIndex = ChunkData[row, col];
+                    
+                    // Check if tileIndex is a valid key in tilesDictionary
+                    if (tilesDictionary.TryGetValue(tileIndex, out Texture2D tileTexture))
+                    {
+                        chunk[row, col] = new TileObject(false, new Vector2(col * tileWidth, row * tileHeight),
+                            tileTexture, tileIndex);
+                    }
+                    else
+                    {
+                        Console.WriteLine($"Tile index {tileIndex} not found in tilesDictionary.");
+                    }
+                }
             }
         }
+
+        
+        public void Draw(SpriteBatch spriteBatch)
+        {
+            for (int row = 0; row < ChunkData.GetLength(0); row++)
+            {
+                for (int col = 0; col < ChunkData.GetLength(1); col++)
+                {
+                    int tileIndex = ChunkData[row, col];
+                    spriteBatch.Draw(tilesDictionary[tileIndex], new Vector2(col * 16, row * 16), Color.White);
+                }
+            }
+        }
+        
     }
-    
-}
